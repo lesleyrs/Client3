366cc818047b9ee3757d85a576fab491c7efb4be
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2025-02-05 17:22:34 +0100

    cleanup

--- src/entity.c
+++ src/entity.c
@@ -27,20 +27,17 @@ void entity_draw_free(Entity *entity, Model *m, int loopCycle) {
                 model_free_share_alpha(m, true);
             }
         }
-    }
-    if (strcmp(entity->type, "npc") == 0) {
+    } else if (strcmp(entity->type, "npc") == 0) {
         NpcEntity *npc = ((NpcEntity *)entity);
         if (npc->pathing_entity.spotanimId == -1 || npc->pathing_entity.spotanimFrame == -1) {
             model_free_share_alpha(m, !npc->type->animHasAlpha);
         } else {
             model_free(m);
         }
-    }
-    if (strcmp(entity->type, "spotanim") == 0) {
+    } else if (strcmp(entity->type, "spotanim") == 0) {
         SpotAnimEntity *spotanim = ((SpotAnimEntity *)entity);
         model_free_share_colored(m, true, !spotanim->type->animHasAlpha, false);
-    }
-    if (strcmp(entity->type, "projectile") == 0) {
+    } else if (strcmp(entity->type, "projectile") == 0) {
         ProjectileEntity *projectile = ((ProjectileEntity *)entity);
         model_free_share_colored(m, true, !projectile->spotanim->animHasAlpha, false);
     }
@@ -50,14 +47,11 @@ Model *entity_draw(Entity *entity, int loopCycle) {
     Model *model = NULL;
     if (strcmp(entity->type, "player") == 0) {
         model = playerentity_draw((PlayerEntity *)entity, loopCycle);
-    }
-    if (strcmp(entity->type, "npc") == 0) {
+    } else if (strcmp(entity->type, "npc") == 0) {
         model = npcentity_draw((NpcEntity *)entity, loopCycle);
-    }
-    if (strcmp(entity->type, "spotanim") == 0) {
+    } else if (strcmp(entity->type, "spotanim") == 0) {
         model = spotanimentity_draw((SpotAnimEntity *)entity, loopCycle);
-    }
-    if (strcmp(entity->type, "projectile") == 0) {
+    } else if (strcmp(entity->type, "projectile") == 0) {
         model = projectileentity_draw((ProjectileEntity *)entity, loopCycle);
     }
     return model;
