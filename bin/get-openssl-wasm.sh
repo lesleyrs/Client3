47212e0007c6c26fdf373c4a17ca2d4af6c3376f
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2025-04-08 17:46:15 +0200

    rm old note

--- bin/get-openssl-wasm.sh
+++ bin/get-openssl-wasm.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 
-# binaries are already provided so this isn't required to run, it's just here to share steps to reproduce them
 # thx to: https://github.com/cryptool-org/openssl-webterm/blob/master/emscr/builds/openssl/build.sh
 git clone https://github.com/openssl/openssl.git && cd openssl || exit 1
 emconfigure ./Configure no-shared no-asm no-threads no-ssl3 no-dtls no-engine no-dso linux-x32 -static
