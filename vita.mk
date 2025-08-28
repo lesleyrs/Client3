e7b580e205e09816ed2dc842b4c759f2c2054a95
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2025-08-29 00:10:43 +0200

    vita: fix make spam by using sort to remove dupe entries

--- vita.mk
+++ vita.mk
@@ -30,7 +30,7 @@ CXXFLAGS += -Wl,-q -std=c++11 -I../common
 SRC_C :=$(call rwildcard, src/, *.c)
 SRC_CPP :=$(call rwildcard, src/, *.cpp)
 
-OBJ_DIRS := $(addprefix out/, $(dir $(SRC_C:src/%.c=%.o))) $(addprefix out/, $(dir $(SRC_CPP:src/%.cpp=%.o)))
+OBJ_DIRS := $(sort $(addprefix out/, $(dir $(SRC_C:src/%.c=%.o))) $(addprefix out/, $(dir $(SRC_CPP:src/%.cpp=%.o))))
 OBJS := $(addprefix out/, $(SRC_C:src/%.c=%.o)) $(addprefix out/, $(SRC_CPP:src/%.cpp=%.o))
 
 LIBS += $(shell $(VITASDK)/arm-vita-eabi/bin/sdl2-config --libs) -lcrypto
@@ -65,7 +65,7 @@ $(PROJECT).elf: $(OBJS)
 	$(CXX) $(CXXFLAGS) $^ $(LIBS) -o $@
 
 $(OBJ_DIRS):
-	mkdir -p $@
+	@mkdir -p $@
 
 out/%.o : src/%.cpp | $(OBJ_DIRS)
 	arm-vita-eabi-g++ -c $(CXXFLAGS) -o $@ $<
