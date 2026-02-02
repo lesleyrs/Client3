54eea93c71abf8c2ca46d9d3649d5546b03f4209
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2026-02-02 23:33:07 +0100

    clean up

--- src/datastruct/hashtable.h
+++ src/datastruct/hashtable.h
@@ -8,8 +8,6 @@ typedef struct {
     Linkable **buckets;
 } HashTable;
 
-#include "linkable.h"
-
 HashTable *hashtable_new(int size);
 Linkable *hashtable_get(HashTable *table, int64_t key);
 void hashtable_put(HashTable *table, int64_t key, Linkable *value);
